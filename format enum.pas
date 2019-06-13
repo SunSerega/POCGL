83435cb7aac72692f66a617e78d1869632575d88
SunSerega <latchenko3@yandex.ru>
2019-06-14 01:11:09 +0300

    format enum.pas

---
+++ format enum.pas
@@ -0,0 +1,131 @@
+﻿program prog;
+{$apptype windows}
+{$reference System.Windows.Forms.dll}
+
+begin
+  try
+    var text := System.Windows.Forms.Clipboard.GetText;
+    
+    var all_lns := text.Remove(#13).ToWords(#10);
+    var lns := all_lns.Where(l->l.StartsWith('#define')).Select(l->
+    begin
+      
+      var ind1 := l.IndexOf(' ')+1;
+      var ind2 := l.IndexOf(' ', ind1);
+      
+      var val := l.Split(new char[](' '), 3, System.StringSplitOptions.RemoveEmptyEntries)[2].Replace('<<','shl').Replace('0x','$');
+      if val.Contains('//') then val := val.Remove(val.IndexOf('//'));
+      if val.Contains('/*') then val := val.Remove(val.IndexOf('/*'));
+      
+      Result := (
+        l.Substring(ind1, ind2-ind1) + ': ',
+        val.Trim(' ', '(', ')'),
+        ''
+      );
+    end).ToArray;
+    
+    var same_letters_sb := new StringBuilder;
+    for var _i := 0 to lns.Max(l->l[0].Length)-1 do
+    begin
+      var i := _i;
+      
+      var m :=
+        lns.Select(t->t[0]).Select(l->i<l.Length?l[i+1]:#0)
+        .GroupBy( ch->ch, ch->ch, (ch,sq)->(ch,sq.Count) )
+        .MaxBy(t->t[1]);
+      
+      if m[1] / lns.Length < 0.8 then break else
+        same_letters_sb += m[0];
+    end;
+    var same_letters := same_letters_sb.ToString;
+    lns := lns.ConvertAll(l->( l[0].StartsWith(same_letters)?l[0].Substring(same_letters.Length):l[0], l[1], '' ));
+    
+    
+    
+    var max_name_l := lns.Max(l->l[0].Length);
+    if max_name_l.IsEven then max_name_l += 1;
+    lns := lns.ConvertAll(l->( l[0].PadRight(max_name_l) , l[1], l[0].Remove(l[0].Length-2) ));
+    
+    var res := new StringBuilder;
+    res += #10;
+    
+    
+    
+    var t_name: string;
+    
+    if all_lns.Any(l->l.Contains('<<')) then
+    begin
+      res += '  Flags = record'#10;
+      t_name := 'cl_bitfield';
+    end else
+    if all_lns[0].ToLower.Contains('info') then
+    begin
+      res += '  InfoType = record'#10;
+      t_name := 'UInt32';
+    end else
+    begin
+      res += '  _____ = record'#10;
+      t_name := 'IntPtr';
+    end;
+    
+    res += $'    public val: {t_name};{#10}';
+    res += $'    public constructor(val: {t_name}) := self.val := val;{#10}';
+    res += $'    public constructor(val: int64) := self.val := val;{#10}';
+    
+    res += '    '#10;
+    
+    
+    
+    foreach var l in lns do
+      res += $'    public static property {l[0]} read new ({l[1]});{#10}';
+    
+    res += '    '#10;
+    
+    foreach var l in lns do
+      res += $'    public property IS_{l[0]}boolean read self = .{l[2]};{#10}';
+    
+    res += '    '#10;
+    
+    foreach var l in lns do
+      res += $'    public property {l[0]}boolean read self and ({l[1]}) <> 0;{#10}';
+    
+    res += '    '#10;
+    
+    
+    
+    res += '    // IS_'#10;
+    res += '    public function ToString: string; override;'#10;
+    res += '    begin'#10;
+    res += '      var res := typeof(_____).GetProperties.Where(prop->prop.PropertyType=typeof(boolean)).Select(prop->(prop.Name,boolean(prop.GetValue(self)))).FirstOrDefault(t->t[1]);'#10;
+    res += '      Result := res=nil?'#10;
+    res += '        $''_____[{self.val}]'':'#10;
+    res += '        res[0].Substring(3);'#10;
+    res += '    end;'#10;
+    
+    res += '    '#10;
+    
+    res += '    // Flags'#10;
+    res += '    public function ToString: string; override;'#10;
+    res += '    begin'#10;
+    res += '      var res := typeof(_____).GetProperties.Select(prop->(prop.Name,boolean(prop.GetValue(self)))).Where(t->t[1]).Select(t->t[0]).ToArray;'#10;
+    res += '      Result := res.Length=0?'#10;
+    res += '        $''_____[{self.val}]'':'#10;
+    res += '        res.JoinIntoString(''+'');'#10;
+    res += '    end;'#10;
+    
+    
+    
+    res += '    '#10;
+    res += '  end;'#10;
+    res += '  ';
+    
+    System.Windows.Forms.Clipboard.SetText(res.ToString);
+    System.Console.Beep;
+  except
+    on e: Exception do
+    begin
+      writeln(e);
+      readln;
+    end;
+  end;
+end.
