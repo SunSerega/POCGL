398cc9c3a0739e35b8f60c1cf442bd7bc5001d08
SunSerega <latchenko3@yandex.ru>
2019-08-11 00:46:28 +0300

    unhotfixed example

--- Rotating Triangle.pas
+++ Rotating Triangle.pas
@@ -43,9 +43,6 @@ begin
     pfd
   ) then raise new InvalidOperationException;
   
-  var context := wgl.CreateContext(Result);
-  if 1 <> wgl.MakeCurrent(Result, context) then raise new InvalidOperationException;
-  
 end;
 
 begin
@@ -59,34 +56,39 @@ begin
   
   f.Load += (o,e)->
   begin
-    var hdc := InitOpenGL(f.Handle);
-    
-    var dy := -Sin(Pi/6) / 2;
-//    var pts := real(0.0).Step(Pi*2/3).Take(3).Select(rot->(Sin(rot), Cos(rot)+dy)).ToArray; //ToDo #2042
-    var pts := Range(0,2).Select(i->i* Pi*2/3 ).Select(rot->(Sin(rot), Cos(rot)+dy)).ToArray;
-    var frame_rot := 0.0;
-    
     System.Threading.Thread.Create(()->
-    while true do
     begin
+      var hdc := InitOpenGL(f.Handle);
       
-      f.Invoke(()->
+      var dy := -Sin(Pi/6) / 2;
+  //    var pts := real(0.0).Step(Pi*2/3).Take(3).Select(rot->(Sin(rot), Cos(rot)+dy)).ToArray; //ToDo #2042
+      var pts := Range(0,2).Select(i->i* Pi*2/3 ).Select(rot->(Sin(rot), Cos(rot)+dy)).ToArray;
+      var frame_rot := 0.0;
+      
+      var context := wgl.CreateContext(hdc);
+      if 1 <> wgl.MakeCurrent(hdc, context) then raise new InvalidOperationException;
+      
+      while true do
       begin
-        gl.Clear(BufferTypeFlags.COLOR_BUFFER_BIT);
-        var rot_k := Cos(frame_rot);
         
-        gl_Deprecated.Begin(PrimitiveType.TRIANGLES.val);
-        gl_Deprecated.Color4f(1,0,0,1); gl_Deprecated.Vertex2f( pts[0][0]*rot_k, pts[0][1] );
-        gl_Deprecated.Color4f(0,1,0,1); gl_Deprecated.Vertex2f( pts[1][0]*rot_k, pts[1][1] );
-        gl_Deprecated.Color4f(0,0,1,1); gl_Deprecated.Vertex2f( pts[2][0]*rot_k, pts[2][1] );
-        gl_Deprecated.&End;
+  //      f.Invoke(()->
+        begin
+          gl.Clear(BufferTypeFlags.COLOR_BUFFER_BIT);
+          var rot_k := Cos(frame_rot);
+          
+          gl_Deprecated.Begin(PrimitiveType.TRIANGLES.val);
+          gl_Deprecated.Color4f(1,0,0,1); gl_Deprecated.Vertex2f( pts[0][0]*rot_k, pts[0][1] );
+          gl_Deprecated.Color4f(0,1,0,1); gl_Deprecated.Vertex2f( pts[1][0]*rot_k, pts[1][1] );
+          gl_Deprecated.Color4f(0,0,1,1); gl_Deprecated.Vertex2f( pts[2][0]*rot_k, pts[2][1] );
+          gl_Deprecated.&End;
+          
+          frame_rot += 0.03;
+          gl.Finish;
+          gdi.SwapBuffers(hdc);
+        end;
         
-        frame_rot += 0.03;
-        gl.Finish;
-        gdi.SwapBuffers(hdc);
-      end);
-      
-      Sleep(16);
+        Sleep(16);
+      end;
     end).Start;
     
   end;
