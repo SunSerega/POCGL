5a69780cd835bab7da01d056ec029b35f462d1ce
SunSerega <latchenko3@yandex.ru>
2019-08-11 17:55:33 +0300

    auto tests updated

--- Tests/Exec/Mtr.Rotate3D.pas
+++ Tests/Exec/Mtr.Rotate3D.pas
@@ -2,7 +2,7 @@
 
 begin
   var v := new Vec3f(1,0,0);
-  var m := Mtr3f.Rotate3Dcw(Vec3f.Create(1,1,1), Pi*2/3).Println;
+  var m := Mtr3f.Rotate3Dcw(Vec3f.Create(1,1,1).Normalized, Pi*2/3).Println;
   loop 4 do
     v := m*v.Println;
   v.Println;
