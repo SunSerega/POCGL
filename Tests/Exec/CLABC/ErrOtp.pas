6a797503cca9583066e0e1756111e40ea12cd812
SunSerega <latchenko3@yandex.ru>
2020-01-06 17:12:34 +0200

    OpenCLABC - вывод исключения из очереди

---
+++ Tests/Exec/CLABC/ErrOtp.pas
@@ -0,0 +1,13 @@
+﻿uses OpenCLABC;
+
+begin
+  try
+    var q := HPQ(()->
+    begin
+      raise new Exception('>>> текст исключения <<<');
+    end);
+    Context.Default.SyncInvoke(q);
+  except
+    on e: Exception do Writeln(e);
+  end;
+end.
