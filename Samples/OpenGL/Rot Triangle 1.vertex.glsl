339e8ef1faf183f9bfaff74ddf3d470fc7cff3b2
SunSerega <latchenko3@yandex.ru>
2020-03-01 19:34:19 +0200

    cleanup because ".0f" in OpenGL shaders

--- Samples/OpenGL/Rot Triangle 1.vertex.glsl
+++ Samples/OpenGL/Rot Triangle 1.vertex.glsl
@@ -9,10 +9,10 @@ void main()
 	
 	gl_Position.x = position.x*rot_k;
 	gl_Position.y = position.y;
-	gl_Position.z = 0;
-	gl_Position.w = 1;
+	gl_Position.z = 0.0f;
+	gl_Position.w = 1.0f;
 	
 	gl_FrontColor.rgb = color;
-	gl_FrontColor.a = 1;
+	gl_FrontColor.a = 1.0f;
 	
 }
