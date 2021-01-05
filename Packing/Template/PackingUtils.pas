8d186eb488dbcff6b709d630dd64d8017c5aa0e1
SunSerega <latchenko3@yandex.ru>
2021-01-05 13:24:56 +0200

    misc

--- Packing/Template/PackingUtils.pas
+++ Packing/Template/PackingUtils.pas
@@ -11,14 +11,21 @@ uses POCGL_Utils in '..\..\POCGL_Utils';
 var _pas_keywords: HashSet<string>;
 function pas_keywords := _pas_keywords;
 
-procedure Init;
-begin
+procedure Init :=
+try
   
   _pas_keywords := ReadLines('Packing\Template\Utils\pas_keywords.dat')
   .Where(l->not string.IsNullOrWhiteSpace(l))
   .Select(l->l.Trim.ToLower)
   .ToHashSet;
   
+except
+  on e: Exception do
+  begin
+    Writeln(DefaultEncoding);
+    Writeln(e);
+    Halt(e.HResult);
+  end;
 end;
 
 begin
