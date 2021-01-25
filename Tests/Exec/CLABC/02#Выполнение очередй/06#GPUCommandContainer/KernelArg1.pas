b8a278b84858491995ae871bb5398ad0b30d2475
SunSerega <latchenko3@yandex.ru>
2021-01-25 05:59:32 +0200

    more KernelArg tests

--- Tests/Exec/CLABC/02#Выполнение очередй/06#GPUCommandContainer/KernelArg1.pas
+++ Tests/Exec/CLABC/02#Выполнение очередй/06#GPUCommandContainer/KernelArg1.pas
@@ -4,23 +4,41 @@ procedure TestArg(arg: KernelArg) :=
 Writeln(arg.GetType);
 
 begin
+  var i: byte;
+  var b := new Buffer(1);
+  
+  
   
   Writeln(#10'>>> Const'#10);
   
   Writeln('Buffer:');
-  TestArg(new Buffer(1));
+  TestArg(KernelArg.FromBuffer(b));
+  TestArg(b);
+  
   Writeln('Record:');
+  TestArg(KernelArg.FromRecord(1));
   TestArg(1);
+  
   Writeln('Ptr:');
   TestArg(KernelArg.FromPtr(System.IntPtr.Zero, System.UIntPtr.Zero));
+  TestArg(@i);
+  
+  
   
   Writeln(#10'>>> Invokable'#10);
   
   Writeln('Buffer:');
-  TestArg(Buffer.Create(1).NewQueue()); //ToDo Лишние (), разобраться когда путаница пройдёт
+  TestArg(KernelArg.FromBufferCQ(b.NewQueue));
+  TestArg(b.NewQueue);
+  
   Writeln('Record:');
+  TestArg(KernelArg.FromRecordCQ(HFQ(()->1)));
   TestArg(HFQ(()->1));
+  
   Writeln('Ptr:');
   TestArg(KernelArg.FromPtrCQ(HFQ(()->System.IntPtr.Zero), System.UIntPtr.Zero));
+  Writeln('OpenCLABCBase_implementation______.Nope >_>');
+  
+  
   
 end.
