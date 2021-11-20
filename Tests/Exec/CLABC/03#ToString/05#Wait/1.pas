1b3e54db49a317aea0c8e1c7a2f9dec37492c3cf
Sun Serega <latchenko3@yandex.ru>
2021-11-20 11:34:15 +0200

    better test

--- Tests/Exec/CLABC/03#ToString/05#Wait/1.pas
+++ Tests/Exec/CLABC/03#ToString/05#Wait/1.pas
@@ -9,13 +9,18 @@ end;
 var M1 := WaitMarker.Create;
 var M2 := WaitMarker.Create;
 
+Test( M1 and M2 );
+Test( M1 or M2 );
+Test( M1 and M1 );
+Test( M1 or M1 );
+Test( (M1 or M2) and (M1 or M2) );
+
+Writeln('='*50);
+Writeln;
+
 Test( WaitFor(M1) );
 Test( (M1+WaitFor(M1)) );
 Test( WaitFor(M1)+M1 );
-Test( WaitFor(M1 and M2) );
-Test( WaitFor(M1 or M2) );
-Test( WaitFor(M1 and M1) );
-Test( WaitFor(M1 or M1) );
 
 Writeln('='*50);
 Writeln;
@@ -24,10 +29,6 @@ var Q0: CommandQueueBase := nil as object;
 Test( Q0.ThenWaitFor(M1) );
 Test( M1+Q0.ThenWaitFor(M1) );
 Test( Q0.ThenWaitFor(M1)+M1 );
-Test( Q0.ThenWaitFor(M1 and M2) );
-Test( Q0.ThenWaitFor(M1 or M2) );
-Test( Q0.ThenWaitFor(M1 and M1) );
-Test( Q0.ThenWaitFor(M1 or M1) );
 
 Writeln('='*50);
 Writeln;
@@ -36,7 +37,3 @@ var mem := new MemorySegment(1);
 Test( mem.NewQueue.AddWait(M1) );
 Test( M1+mem.NewQueue.AddWait(M1) );
 Test( mem.NewQueue.AddWait(M1)+M1 );
-Test( mem.NewQueue.AddWait(M1 and M2) );
-Test( mem.NewQueue.AddWait(M1 or M2) );
-Test( mem.NewQueue.AddWait(M1 and M1) );
-Test( mem.NewQueue.AddWait(M1 or M1) );
