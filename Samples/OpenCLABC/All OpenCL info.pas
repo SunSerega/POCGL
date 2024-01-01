d232a9245471a4a6ee6b8b2a6540cfb6289b8118
Sun Serega <sunserega2@gmail.com>
2024-01-01 13:42:24 +0200

    "All OpenCL info" example

---
+++ Samples/OpenCLABC/All OpenCL info.pas
@@ -0,0 +1,13 @@
+﻿## uses OpenCLABC;
+
+foreach var pl in CLPlatform.All do
+begin
+  Println(pl);
+  Println(pl.Properties);
+  foreach var dvc in CLDevice.GetAllFor(pl, clDeviceType.DEVICE_TYPE_ALL) do
+  begin
+    Println(dvc);
+    Println(dvc.Properties);
+  end;
+end;
+
