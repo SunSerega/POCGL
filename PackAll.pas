0f6f93b0fcbf07319c1705ba3797382af83465fc
Sun Serega <sunserega2@gmail.com>
2024-01-12 23:00:01 +0200

    compile Dummy.pas by default

--- PackAll.pas
+++ PackAll.pas
@@ -257,13 +257,15 @@ var AllStages := HSet(
       if mn       in CurrentStages then Result += EventTask(ModulePackingStage.GetModulePackEv(mn));
       if mn+'ABC' in CurrentStages then Result += EventTask(ModulePackingStage.GetModulePackEv(mn+'ABC'));
       
-      // If no modules are selected, but compile stage is:
-      // - Compile everything except for Dummy
-      if not ModuleStages.Any and FileExists($'Modules/{mn}ABC.pas') then
-        Result += CompTask($'Modules.Packed/{mn}ABC.pas') else
       if mn+'ABC' in CurrentStages then
         Result += CompTask($'Modules.Packed/{mn}ABC.pas') else
       if mn in CurrentStages then
+        Result += CompTask($'Modules.Packed/{mn}.pas') else
+      if not IsPackingAllModules then
+        exit;
+      
+      if FileExists($'Modules/{mn}ABC.pas') then
+        Result += CompTask($'Modules.Packed/{mn}ABC.pas') else
         Result += CompTask($'Modules.Packed/{mn}.pas');
       
     end;
